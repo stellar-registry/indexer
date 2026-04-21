99f10cd7405c97eebe0a10d55b692d62cc75cf84
Willem Wyndham <willem@ahalabs.dev>
2026-04-21 11:04:10 -0400

    chore: remove superseded AWS CDK/Lambda stack and dead configs

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,6 @@ const baseTransform = { '^.+\\.tsx?$': 'ts-jest' };
 module.exports = {
   projects: [
     {
-      displayName: 'unit',
-      testEnvironment: 'node',
-      roots: ['<rootDir>/test/unit'],
-      testMatch: ['**/*.test.ts'],
-      transform: baseTransform,
-    },
-    {
       displayName: 'integration',
       testEnvironment: 'node',
       roots: ['<rootDir>/test/integration'],
