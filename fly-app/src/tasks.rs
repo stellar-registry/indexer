e1c060bc474648c0376e3f73c07eb624b8ed41d6
Kolos Niedermayer <38922266+b4sen@users.noreply.github.com>
2026-07-16 16:38:26 +0300

    extract constructor args

--- fly-app/src/tasks.rs
+++ fly-app/src/tasks.rs
@@ -1,5 +1,5 @@
 use sqlx::PgPool;
-use stellar_xdr::curr::{ScMetaEntry, ScMetaV0};
+use stellar_xdr::curr::{ScMetaEntry, ScMetaV0, ScSpecEntry};
 
 fn parse_wasm_meta(wasm: &[u8]) -> Result<serde_json::Map, soroban_meta::read::FromWasmError> {
     let meta = soroban_meta::read::from_wasm(&wasm);
@@ -24,7 +24,30 @@ fn parse_wasm_spec(wasm: &[u8]) -> Result<serde_json::Map, soroban_spec::read::F
     match spec {
         Ok(entries) => {
             let mut obj = serde_json::Map::new();
-            for entry in entries {}
+            let constructor = entries.into_iter().find_map(|entry| match entry {
+                ScSpecEntry::FunctionV0(f) if f.name.to_utf8_string_lossy() == "__constructor" => {
+                    Some(f)
+                }
+                _ => None,
+            });
+            constructor.map(|f| {
+                let spec = ConstructorSpec {
+                    doc: Some(f.doc.to_utf8_string_lossy()),
+                    inputs: f
+                        .inputs
+                        .iter()
+                        .map(|i| FunctionInput {
+                            doc: i.doc.to_utf8_string_lossy(),
+                            name: i.name.to_utf8_string_lossy(),
+                            type_: i.type_.clone(),
+                        })
+                        .collect(),
+                };
+                obj.insert(
+                    "constructor".to_string(),
+                    serde_json::to_value(value).unwrap(),
+                );
+            });
             return Ok(obj);
         }
         Err(e) => Err(e),
